003fb79a4a3503bcbe74a1060899b2776d6cf8f1
linyt <linyituo@abecool.com>
2016-09-12 23:18:00 +0800

    新增好多控件

--- common/index.js
+++ common/index.js
@@ -2,4 +2,20 @@ $(function() {
     $(".pageOne").click(function(){
         window.location.href="modules/tree/tree.html";
     });
+    $(".pageTwo").click(function(){
+        window.location.href="http://www.html5tricks.com/demo/html5-canvas-choujiang/index.html";
+    });
+       $(".page3").click(function(){
+        window.location.href="http://www.html5tricks.com/demo/pure-css3-elephant-animation/index.html";
+    });
+    $(".page4").click(function(){
+        window.location.href="http://www.html5tricks.com/demo/html5-guide-tooltip/index.html";
+    });
+    $(".page5").click(function(){
+        window.location.href="http://js.itivy.com/jiaoben259/index.html";
+    });
+    $(".page5").click(function(){
+        window.location.href="http://www.html5tricks.com/demo/html5-css3-clock-with-date/index.html";
+    });
+
 });
